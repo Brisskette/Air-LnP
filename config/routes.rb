5289dc8f0268f4a81bbfa4a8ac0e42b36abf6527
Antoine Brissy <antoinebrissy@gmail.com>
2018-05-14 21:13:35 +0200

    creation des routes pour lands et bookings

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+
+  resources :lands, only: [ :new, :create, :show, :index ]
+
+  resources :lands do
+    resources :bookings, only: [ :new, :create ]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

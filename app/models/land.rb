95dbaa27ec3e9fa2af8e62464610b16bd9197a1f
Jose Veiga <veiga.jose@free.fr>
2018-05-16 16:20:22 +0200

    show update

--- app/models/land.rb
+++ app/models/land.rb
@@ -1,6 +1,6 @@
 class Land < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings , dependent: :destroy
   has_many :reviews , through: :bookings
   mount_uploader :photo, PhotoUploader
 
